c45f2087d5f47942b7638bb14a72ca3aa7f5eb8b
lomnom <zhaoxiong.ang@gmail.com>
2022-09-06 17:11:02 +0800

    undo wrong delete

--- CvDemo.py
+++ CvDemo.py
@@ -1,31 +1,31 @@
-import TermUI as tui
 import TermCanvas as tc
 import Terminal as trm
 
 @tc.canvasApp
-def main(cnv):
-	root=tui.Root(
-		cnv,
-		tui.VStack(
-			tui.Squisher(
-				tui.Box(
-					tui.Text("*_HELLO WORLD_*"),
-					tui.Lines.double
-				),
-				squishX=15,
-				squishY=7
-			),
-			tui.Squisher(
-				tui.Box(
-					tui.Text("Lmao"),
-					tui.Lines.thin
-				),
-				squishX=13,
-				squishY=3
-			)
-		)
-	)
-	root.render()
-	cnv.render()
+def main(term):
 	keyIter=trm.keys()
-	next(keyIter)
+	key=None
+ 
+	while True:
+		term.sprint("\b[blue]*HE*|LLO| \f[magenta]\b[green]*WO*|RLD|!\b\f[default]\n")
+		term.sprint("Effects:\n")
+		term.sprint("    - *BOLD*\n")
+		term.sprint("    - |uline|\n")
+		term.sprint("    - *|BOLD+uline|*\n")
+		term.sprint("Colors:\n")
+		term.sprint("    - \f[red]re\f[default]\b[red]d\b[default]\n")
+		term.sprint("    - \f[green]gr\f[default]\b[green]een\b[default]\n")
+		term.sprint("    - \f[yellow]yel\f[default]\b[yellow]low\b[default]\n")
+		term.sprint("    - \f[blue]bl\f[default]\b[blue]ue\b[default]\n")
+		term.sprint("    - \f[magenta]mage\f[default]\b[magenta]nta\b[default]\n")
+		term.sprint("    - \f[cyan]cy\f[default]\b[cyan]an\b[default]\n")
+		term.sprint("    - \f[default]defa\f[default]\b[default]ult\b[default]\n")
+		if key:
+			term.sprint(f"Received keyboard key '{key}'\n")
+		term.render()
+		term.cursor.goto(0,0)
+		term.clear()
+ 
+		key=next(keyIter)
+		if key=="ctrl z":
+			break

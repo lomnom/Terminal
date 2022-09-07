b9c3914111d0b57c11dd382812a1b6283954a1d3
lomnom <zhaoxiong.ang@gmail.com>
2022-09-07 12:13:31 +0800

    add init

--- TermUI.py
+++ TermUI.py
@@ -20,6 +20,35 @@ class Lines:
 	double=LineSet('═',"║","╔","╗","╚","╝")
 	thinC=LineSet('─','│','╭','╮','╰','╯')
 
+class Gradients:
+	block=[" ","░","▒","▓","█"]
+	horizbar=[" ","▏","▎","▍","▌","▋","▊","▉","█"]
+	vertbar=[" ","▁","▂","▃","▄","▅","▆","▇","█"]
+
+blocks={
+	# 0bXXXX
+	#   |||^bottomRight
+	#   ||^bottomLeft
+	#   |^topRight
+	#   ^topLeft
+	0b0010:"▖",
+	0b0001:"▗",
+	0b1000:"▘",
+	0b0100:"▝",
+	0b1011:"▙",
+	0b1001:"▚",
+	0b0110:"▞",
+	0b1110:"▛",
+	0b1101:"▜",
+	0b0111:"▟",
+	0b1100:"▀",
+	0b0011:"▄",
+	0b1010:"▌",
+	0b0101:"▐",
+	0b1111:"█",
+	0b0000:" "
+}
+
 class Element:
 	index=None
 	ridgid=False #(grows to fill rows, cols), does not check parent size
---
+++ __init__.py
@@ -0,0 +1,3 @@
+import Terminal as trm
+import TermCanvas as tc
+import TermUI as ui
